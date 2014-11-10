a4ae2f8061aa9b23f8d6a99426da0b7a3b7b0343
mbrightbill <brightbill.mt@gmail.com>
2014-11-10 13:40:59 -0800

    Initial Commit

---
+++ stackOClient/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  stackOClient
+//
+//  Created by Matthew Brightbill on 11/10/14.
+//  Copyright (c) 2014 Matthew Brightbill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
